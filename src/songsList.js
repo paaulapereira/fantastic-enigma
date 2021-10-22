1a85c4846da065889aa985733b710fcd3b6c96c8
paula_pereira <paula.decosta@alumnos.uneatlantico.es>
2021-10-22 09:55:42 +0200

    nuevas canciones

--- src/songsList.js
+++ src/songsList.js
@@ -32,26 +32,6 @@ function chillHop() {
       active: false,
     },
     {
-      name: "Beaver Creek",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
-      artist: "Aso, Middle School, Aviino",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=10075",
-      color: ["#2ab3bf", "#205950"],
-      id: uuidv4(),
-      active: false,
-    },
-    {
-      name: "Daylight",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
-      artist: "Aiguille",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9272",
-      color: ["#EF8EA9", "#ab417f"],
-      id: uuidv4(),
-      active: false,
-    },
-    {
       name: "Keep Going",
       cover:
         "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
@@ -82,42 +62,12 @@ function chillHop() {
       active: false,
     },
     {
-      name: "Reflection",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
-      artist: "Swørn",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9228",
-      color: ["#CD607D", "#c94043"],
-      id: uuidv4(),
-      active: false,
-    },
-    {
       name: "Under the City Stars",
       cover:
         "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
       artist: "Aso, Middle School, Aviino",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=10074",
       color: ["#2ab3bf", "#205950"],
-      id: uuidv4(),
-      active: false,
-    },
-    {
-      name: "Jazz Cabbage",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/06/49f6e32ca521fbad46a1b281e3893cf6254bf11d-1024x1024.jpg",
-      artist: "Ian Ewing, Strehlow",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9363",
-      color: ["#FDF0DD", "#BA4A46"],
-      id: uuidv4(),
-      active: false,
-    },
-    {
-      name: "Today Feels Like Everyday",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2021/02/08cbb0848f669e1bd8b5a5152c4b7d20edf1b499-1024x1024.jpg",
-      artist: "Mama Aiuto",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=12128",
-      color: ["#F4AD97", "#4A2222"],
       id: uuidv4(),
       active: false,
     },
@@ -138,16 +88,6 @@ function chillHop() {
       artist: "Sleepy Fish",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=10017",
       color: ["#86BCCA", "#22607C"],
-      id: uuidv4(),
-      active: false,
-    },
-    {
-      name: "Lagoons",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/06/49f6e32ca521fbad46a1b281e3893cf6254bf11d-1024x1024.jpg",
-      artist: "Strehlow, Chris Mazuera",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=8266",
-      color: ["#FDF0DD", "#BA4A46"],
       id: uuidv4(),
       active: false,
     },
